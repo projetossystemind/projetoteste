6ef1a1e9d3e2c0d79cd3ad13d9e1402f45733bbc
reginaldo.santos <reginaldo.santos@CDDTI01.delfin.com.br>
2017-01-10 13:57:21 -0300

    Esse é minha terceira commit

--- NossoProjetoTeste/index.php
+++ NossoProjetoTeste/index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+++
 -->
 <html>
     <head>
@@ -13,6 +11,7 @@ and open the template in the editor.
         <?php
         // put your code here
         ?>
-        Este é minha modificação.
+        Esta é minha modificação 4
+        .
     </body>
 </html>
